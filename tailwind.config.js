230770da76a2e34373ec07b31eddbf21c3569ef0
Chisomo Chiweza <chisomochiweza@proton.me>
2023-08-06 19:44:44 +0200

    Build homepage: - Install Tailwindcss - Setup Tailwindcss successfully

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./site/*.html",
+    "./site/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
